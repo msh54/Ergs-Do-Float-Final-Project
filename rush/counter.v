7423b1a6b5ca84abe4d36aa23f462bcb4a31ea53
Rberg77 <rb265@duke.edu>
2018-12-04 16:42:59 -0500

    Laid out foundation for rusher module, will wait for Nathaniel's help with it tomorrow

--- rush/counter.v
+++ rush/counter.v
@@ -1,6 +1,84 @@
-module counter(start_count1,start_count2,end_count,start_recovery, start_ref1, start_ref2, end_ref3, end_recovery,clk,reset);
-input  clk,start_drive,start_recovery, start_ref1, start_ref2, end_ref3, end_recovery;
+module counter(start_count1,start_count2,end_count,start_recovery, start_ref1, start_ref2, end_ref3,rush,end_recovery,clk,reset);
+input  clk,start_recovery, start_ref1, start_ref2, end_ref1, end_recovery;
 output[31:0] start_count1,start_count2,end_count;
+output[31:0] rush;
+
+reg start_recovery_reg, start_ref1_reg, start_ref2_reg, end_ref1_reg, end_recovery_reg;
+reg sc1_enable, sc2_enable, ec_enable;
+integer average;
+
+initial
+begin
+	start_recovery = 1'b0;
+	start_ref1 = 1'b0;
+	start_ref2 = 1'b0;
+	end_ref1 = 1'b0;
+	end_recovery = 1'b0;
+	sc1_enable = 1'b0;
+	sc2_enable = 1'b0;
+	ec_enable = 1'b0;
+end
+
+always @ (negedge clk)
+begin
+	if(start_recovery && start_recovery != start_recovery_reg)
+	begin
+		sc1_enable = 1'b1;
+		sc2_enable = 1'b0;
+		ec_enable = 1'b0;
+	end
+	else if(start_ref1 && start_ref1 != start_ref1_reg)
+	begin
+		sc1_enable = 1'b0;
+		sc2_enable = 1'b1;
+		ec_enable = 1'b0;
+	end
+	else if(start_ref2 && start_ref2 != start_ref2_reg)
+	begin
+		sc1_enable = 1'b0;
+		sc2_enable = 1'b0;
+		ec_enable = 1'b0;
+	end
+	else if(end_ref1 && end_ref1 != end_ref1_reg)
+	begin
+		sc1_enable = 1'b0;
+		sc2_enable = 1'b0;
+		ec_enable = 1'b1;
+	end
+	else if(end_recovery && end_recovery != end_recovery_reg)
+	begin
+		sc1_enable = 1'b0;
+		sc2_enable = 1'b0;
+		ec_enable = 1'b0;
+		
+		average = start_count1 + start_count2 / 2;
+		rush = average/ec_count;
+		
+		start_count1 = 0;
+		start_count2 = 0;
+		start_count3 = 0;
+	end
+	
+	if(sc1_enable)
+	begin
+		start_count1 = start_count1 + 1;
+	end
+	
+	if(sc2_enable)
+	begin
+		start_count2 = start_count2 + 1;
+	end
+	
+	if(ec_enable)
+	begin
+		ec_enable = ec_enable + 1;
+	end
+	
+	
+	
+	
+	
+end
 
 
 endmodule
--- rush/rusher.v
+++ rush/rusher.v
@@ -1,4 +1,78 @@
-module rusher(rush,start_recovery, start_ref1, start_ref2, end_ref3, end_recovery,clk);
+module rusher(start_count1,start_count2,end_count,start_recovery, start_ref1, start_ref2, end_ref1,rush,end_recovery,clk);
+input  clk,start_recovery, start_ref1, start_ref2, end_ref1, end_recovery;
+output[31:0] start_count1,start_count2,end_count;
+output[31:0] rush;
+
+reg start_recovery_reg, start_ref1_reg, start_ref2_reg, end_ref1_reg, end_recovery_reg;
+reg sc1_enable, sc2_enable, ec_enable;
+integer average;
+
+initial
+begin
+	sc1_enable = 1'b0;
+	sc2_enable = 1'b0;
+	ec_enable = 1'b0;
+end
+
+always @ (negedge clk)
+begin
+	if(start_recovery && start_recovery != start_recovery_reg)
+	begin
+		sc1_enable = 1'b1;
+		sc2_enable = 1'b0;
+		ec_enable = 1'b0;
+	end
+	else if(start_ref1 && start_ref1 != start_ref1_reg)
+	begin
+		sc1_enable = 1'b0;
+		sc2_enable = 1'b1;
+		ec_enable = 1'b0;
+	end
+	else if(start_ref2 && start_ref2 != start_ref2_reg)
+	begin
+		sc1_enable = 1'b0;
+		sc2_enable = 1'b0;
+		ec_enable = 1'b0;
+	end
+	else if(end_ref1 && end_ref1 != end_ref1_reg)
+	begin
+		sc1_enable = 1'b0;
+		sc2_enable = 1'b0;
+		ec_enable = 1'b1;
+	end
+	else if(end_recovery && end_recovery != end_recovery_reg)
+	begin
+		sc1_enable = 1'b0;
+		sc2_enable = 1'b0;
+		ec_enable = 1'b0;
+		
+		assign average = start_count1 + start_count2 / 2;
+		assign rush = (end_count != 32'b0) ? average/end_count : 32'b0;
+		
+		assign start_count1 = 0;
+		assign start_count2 = 0;
+	end
+	
+	if(sc1_enable)
+	begin
+		assign start_count1 = start_count1 + 1;
+	end
+	
+	if(sc2_enable)
+	begin
+		assign start_count2 = start_count2 + 1;
+	end
+	
+	if(ec_enable)
+	begin
+		assign ec_enable = ec_enable + 1;
+	end
+	
+	
+	
+	
+	
+end
 
 
 endmodule
