e6b4eb76731a50ba3d242a7e087dc0d442b61306
Rberg77 <rb265@duke.edu>
2018-12-04 15:57:09 -0500

    initial files for the rusher

---
+++ rush/counter.v
@@ -0,0 +1,6 @@
+module counter(start_count1,start_count2,end_count,start_recovery, start_ref1, start_ref2, end_ref3, end_recovery,clk,reset);
+input  clk,start_drive,start_recovery, start_ref1, start_ref2, end_ref3, end_recovery;
+output[31:0] start_count1,start_count2,end_count;
+
+
+endmodule
---
+++ rush/rusher.v
@@ -0,0 +1,4 @@
+module rusher(rush,start_recovery, start_ref1, start_ref2, end_ref3, end_recovery,clk);
+
+
+endmodule
